bff9d4ce7fa20ea9450734fd22c319e00b162537
jennl97 <jennhott97@gmail.com>
2017-09-05 10:26:42 -0500

    Initial commit

---
+++ src/Version_Control.java
@@ -0,0 +1,10 @@
+public class Version_Control {
+          public static void main(String[] args){
+            System.out.println("Hello world, I'm Jenn!");
+            System.out.println("I work full time in Madison");
+            System.out.println("I have 4 children");
+            System.out.println("Today is my day off!");
+            System.out.println("I am super tired and want a nap");
+            System.exit(0);
+        }
+    }
